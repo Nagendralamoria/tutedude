295de7643f633db87f53256004b0990843c4080a
Nagendra Lamoria <nagendrasinghlamoriya@gmail.com>
2023-01-15 08:21:44 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 import Referandearn from './Pages/Referandearn';
 import Friendsrefered from './Pages/Friendsrefered';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
         <>
-         <Referandearn/>
-          <Friendsrefered/> 
+         {/* <Referandearn/>
+          <Friendsrefered/> */}
+      
+      <Routes>
+        <Route path="/" element={<Referandearn/>} />
+        <Route path="/friendsrefer" element={<Friendsrefered/>} />
+      </Routes>
+
          
           </>
        
